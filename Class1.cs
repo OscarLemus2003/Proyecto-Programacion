e3485f3d1f1bd283d97da26b054b6debe1c24cae
oscar <oscar@gmail.com>
2022-02-18 21:05:55 -0600

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class aprendiendo
+{
+
+    static void Main(string[] args)
+    {
+
+        long a, b, c, d, e;
+        Console.WriteLine("Ingrese el Primer Numero");
+        Console.ReadLine(a);
+        Console.WriteLine("Ingrese el Segundo Numero");
+        Console.ReadLine(b);
+
+        c = a + b;
+        d = a - c;
+        e = b * d;
+
+        Console.WriteLine("Los Resultados Son" "a" + "b" = c, "a" - "b" = d, "a" * "b" = e;
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class aprendiendo
+{
+    static void Main(string[] args)
+    {
+
+
+        byte edad;
+        Console.WriteLine("ingrese su edad");
+        edad = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("usted no aparenta " + edad + "años");
+        Console.WriteLine();
+        Console.ReadLine();
+
+
+    }
+
+
+}
